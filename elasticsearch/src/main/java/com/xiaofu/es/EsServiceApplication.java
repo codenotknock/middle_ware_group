f12d19ecec9a1d2f3d1a402ba87744b0dfd64244
xiaofu <xiaofu@dtstack.com>
2024-06-01 01:33:33 +0800

    FEAT: es模块初始化

---
+++ elasticsearch/src/main/java/com/xiaofu/es/EsServiceApplication.java
@@ -0,0 +1,17 @@
+package com.xiaofu.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class EsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsServiceApplication.class, args);
+    }
+
+}
